3e503c089244a599463b62d074349f0ecb795c1f
Seb Martin <seb.d.martin@gmail.com>
2025-06-12 23:27:59 +0000

    Initial service implementation

---
+++ src/ve_renogy_rover/device_info.py
@@ -0,0 +1,73 @@
+import logging
+import os
+import json
+from dataclasses import dataclass
+from random import randrange
+
+from pyrover.renogy_rover import RenogyRoverController as Rover
+
+PRODUCT_NAME = "Renogy Rover MPPT"
+
+@dataclass
+class DeviceInfo:
+    product_name: str = PRODUCT_NAME
+    custom_name: str = PRODUCT_NAME
+    serial: str = "RNG-CTRL-RVR"
+    firmware_version: str = "0.0.0"
+    hardware_version: str = "0.0.0"
+
+    @staticmethod
+    def from_file(path: str) -> 'DeviceInfo':
+        """Load DeviceInfo from a JSON file."""
+        try:
+            with open(path, "r") as f:
+                return DeviceInfo.from_dict(json.load(f))
+        except (FileNotFoundError, json.JSONDecodeError):
+            return DeviceInfo()
+
+    @staticmethod
+    def from_dict(data: dict) -> 'DeviceInfo':
+        """Create DeviceInfo from a dictionary."""
+        args = {
+            key: value
+            for key, value in data.items()
+            if key in DeviceInfo.__dataclass_fields__
+        }
+        args.setdefault("serial", f"RNG-CTRL-RVR_{randrange(1000, 9999)}")
+        return DeviceInfo(**args)
+
+    def to_file(self, path: str):
+        """Save DeviceInfo to a JSON file."""
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        # Write atomically: write to a temp file then rename it
+        temp_path = path + ".tmp"
+        with open(temp_path, "w") as f:
+            json.dump(self.to_dict(), f, indent=2)
+        os.replace(temp_path, path)
+
+    def to_dict(self) -> dict:
+        """Convert the DeviceInfo to a dictionary."""
+        return {
+            "serial": self.serial,
+            "firmware_version": self.firmware_version,
+            "hardware_version": self.hardware_version,
+            "custom_name": self.custom_name,
+        }
+
+    def update_from_device(self, rover: Rover):
+        """Update DeviceInfo from the Renogy Rover device."""
+        try:
+            product = rover.product_model()
+            serial = rover.serial_number()
+            self.serial = f"{product}_{serial}"
+        except Exception:
+            logging.warning("Failed to read product model andserial number, using default.")
+        try:
+            self.firmware_version = rover.software_version()
+        except Exception:
+            logging.warning("Failed to read firmware version, using default.")
+        try:
+            self.hardware_version = rover.hardware_version()
+        except Exception:
+            logging.warning("Failed to read hardware version, using default.")
---
+++ src/ve_renogy_rover/rover_service.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+
+"""
+Renogy MPPT example see:
+https://github.com/sstoops/dbus-renogy-dcc/blob/main/dbus-renogy-dcc.py
+"""
+
+import logging
+import sys
+
+from ve_renogy_rover.device_info import DeviceInfo
+from pyrover.renogy_rover import RenogyRoverController as Rover
+
+# Add the paths to some system packages
+sys.path.insert(1, "/usr/lib/python3.8/site-packages")
+sys.path.insert(1, "/opt/victronenergy/dbus-systemcalc-py/ext/velib_python")
+from gi.repository import GLib
+from vedbus import VeDbusService
+
+VERSION = "v0.1.0"
+CUSTOM_PRODUCT_ID = 0xF102 # Custom product ID for Renogy Rover MPPT, randomly chosen
+UPDATE_INTERVAL = 1000  # milliseconds
+SETTINGS_PATH = "/data/renogy/rover.json"
+
+
+class RoverService(object):
+    """
+    A D-Bus service for the Renogy Rover MPPT solar charger to be used with Victron's Venus OS.
+    This service communicates with a Renogy Rover MPPT device connected via USB and provides
+    publishes information about the device such as voltage, current, and other parameters
+    to the D-Bus system, allowing it to be monitored and controlled via the Victron system.
+    """
+
+    def __init__(self, tty: str):
+        self._tty = tty
+        self._device_instance = None
+        self._rover = None
+
+        self.device_info = DeviceInfo.from_file(SETTINGS_PATH)
+
+        self._register_dbus_service()
+
+    @property
+    def tty(self) -> str:
+        """Return the tty name."""
+        return self._tty
+
+    @property
+    def usb_number(self) -> int:
+        """
+        Return the USB number extracted from the tty name.
+        e.g.
+          ttyUSB0 -> 0, ttyUSB1 -> 1, etc.
+          /dev/ttyUSB0 -> 0, /dev/ttyUSB1 -> 1, etc.
+        """
+        parts = self._tty.lower().split("ttyusb")
+        if len(parts) == 2 and parts[1].isdigit():
+            return int(parts[1])
+        else:
+            raise ValueError(f"Unsupported TTY name: {self._tty}")
+
+    @property
+    def service_name(self) -> str:
+        return f"com.victronenergy.solarcharger.{self._tty}"
+
+    @property
+    def connection(self) -> str:
+        """
+        Return the connection string for the service.
+        This is used in the /Mgmt/Connection path.
+        """
+        return f"Renogy Rover MPPT on USB{self.usb_number}"
+
+    @property
+    def device_instance(self) -> int:
+        """
+        Return the device instance based on the tty name. Only supports USB for now.
+          /dev/ttyUSBx: 288 + x
+        """
+        if self._device_instance is None:
+            self._device_instance = 288 + self.usb_number
+        return self._device_instance
+
+    @property
+    def rover(self) -> Rover:
+        if not self._rover:
+            self._rover = Rover(address=1, port=self._tty)
+        return self._rover
+
+    def _register_dbus_service(self):
+        # Get a few static values from the device
+        self.device_info.update_from_device(self.rover)
+
+        self._dbusservice = VeDbusService(self.service_name)
+
+        # Create the management objects, as specified in the ccgx dbus-api document
+        self._dbusservice.add_path("/Mgmt/ProcessName", __file__)
+        self._dbusservice.add_path("/Mgmt/ProcessVersion", VERSION)
+        self._dbusservice.add_path("/Mgmt/Connection", self.connection)
+
+        # Create the mandatory objects
+        self._dbusservice.add_path("/DeviceInstance", self.device_instance)
+        self._dbusservice.add_path("/ProductId", CUSTOM_PRODUCT_ID)
+        self._dbusservice.add_path("/ProductName", self.device_info.product_name)
+        self._dbusservice.add_path(
+            "/CustomName",
+            self.device_info.custom_name,
+            writeable=True,
+            onchangecallback=self._on_custom_name_change
+        )
+        self._dbusservice.add_path("/Serial", self.device_info.serial)
+        self._dbusservice.add_path("/FirmwareVersion", self.device_info.firmware_version)
+        self._dbusservice.add_path("/HardwareVersion", self.device_info.hardware_version)
+        self._dbusservice.add_path("/Connected", 1)
+
+        # https://github.com/victronenergy/venus/wiki/dbus#solar-chargers
+        paths = {
+            "/NrOfTrackers": 1,  # Rovers are single tracker devices
+            "/Pv/V": 0,  # Voltage in Volts, exists only for single tracker devices
+            "/Pv/I": 0,  # Power in Watts, exists only for single tracker devices
+            "/Yield/Power": 0,  # Power in Watts, total yield power
+            "/MppOperationMode": 2,  # MPPT Tracker active
+            "/Dc/0/Voltage": 0,  # Actual battery voltage
+            "/Dc/0/Current": 0,  # Actual battery charging current
+            "/Mode": 1,   # 1=On; 4=Off
+            "/DeviceOffReason": 0,   # Bitmask indicating the reason(s) that the MPPT is in Off State
+        }
+        for path, initial in paths.items():
+            self._dbusservice.add_path(path, initial, writeable=False)
+
+        self._dbusservice.register()
+
+        # Callback to update the values periodically
+        GLib.timeout_add(UPDATE_INTERVAL, self._update_path_values)
+
+
+    def _update_path_values(self):
+        rover = self.rover
+        updates = {
+            "/Pv/V":rover.solar_voltage(),
+            "/Pv/I":rover.charging_power(),
+            "/Yield/Power":rover.solar_voltage() * rover.solar_current(),
+            "/Dc/0/Voltage":rover.battery_voltage(),
+            "/Dc/0/Current":rover.charging_current(),
+        }
+
+        with self._dbusservice as s:
+            for path, value in updates.items():
+                s[path] = value
+                logging.debug("%s: %s" % (path, s[path]))
+        return True
+
+    def _on_custom_name_change(self, path, value):
+        """
+        Set the custom name for the device.
+        This is a writeable path that can be changed via the dbus.
+        """
+        self.device_info.custom_name = value
+        self.device_info.to_file(SETTINGS_PATH)
+        return True
+
+def main():  # For debugging purposes, run this script directly
+    logging.basicConfig(level=logging.DEBUG)
+
+    from dbus.mainloop.glib import DBusGMainLoop
+    # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
+    DBusGMainLoop(set_as_default=True)
+
+    RoverService(tty="ttyUSB0")
+
+    logging.info("Connected to dbus, and switching over to GLib.MainLoop() (= event based)")
+    mainloop = GLib.MainLoop()
+    mainloop.run()
+
+
+if __name__ == "__main__":
+    main()
